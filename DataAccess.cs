38dc915778818a78fd075dbf322002b1fa8c0ff9
pezongod <pezongod@gmail.com>
2019-07-24 11:07:29 +0200

    --

--- DataAccess.cs
+++ DataAccess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,68 @@ namespace Inventory
 {
     public class DataAccess
     {
-        
+        private const string conString = "Server=(localdb)\\mssqllocaldb; Database=Inventory";
+
+        public List<BlogPost> GetAllInventoriesBrief()
+        {
+            var sql = @"SELECT [Id], [Author], [Title]
+                        FROM BlogPost";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                var list = new List<BlogPost>();
+
+                while (reader.Read())
+                {
+                    var bp = new BlogPost
+                    {
+                        Id = reader.GetSqlInt32(0).Value,
+                        Author = reader.GetSqlString(1).Value,
+                        Title = reader.GetSqlString(2).Value
+                    };
+                    list.Add(bp);
+                }
+
+                return list;
+
+            }
+        }
+
+        public BlogPost GetPostById(int postId)
+        {
+            var sql = @"SELECT [Id], [Author], [Title]
+                        FROM BlogPost 
+                        WHERE Id=@Id";
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                command.Parameters.Add(new SqlParameter("Id", postId));
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    var bp = new BlogPost
+                    {
+                        Id = reader.GetSqlInt32(0).Value,
+                        Author = reader.GetSqlString(1).Value,
+                        Title = reader.GetSqlString(2).Value
+                    };
+                    return bp;
+
+                }
+
+                return null;
+
+            }
+        }
+
     }
 }
