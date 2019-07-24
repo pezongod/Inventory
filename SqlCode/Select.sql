990ec09077ce04982f83385401f18f4ecc9b993a
Ciwifrukt <v.strandberg@outlook.com>
2019-07-24 11:06:35 +0200

    SqlFiles Added

---
+++ SqlCode/Select.sql
@@ -0,0 +1,19 @@
+﻿
+--USE BlogPostDemo
+
+--SELECT * FROM Tags
+
+--SELECT * FROM BlogPost
+
+--Select Tags.Id, Tags.Name 
+--                FROM Tags 
+--                WHERE Tags.Name = 'C#'
+
+--						SELECT BlogPost.Id, BlogPost.Author, BlogPost.Title, BlogPost.Content, Tags.Name, Comment.Id, Comment.CommentText
+--                        FROM BlogPost
+--                        join PostTags on PostTags.PostId = BlogPost.Id
+--                        join Tags on Tags.Id = PostTags.TagId
+--                        join Comment on Comment.PostId = BlogPost.Id
+--                        WHERE BlogPost.Id=1
+
+--						Select * From Comment
---
+++ SqlCode/recreate.sql
@@ -0,0 +1,26 @@
+﻿----USE master
+--CREATE DATABASE BlogPostDemo 
+----drop database BlogPostDemo
+--USE BlogPostDemo
+
+--DROP TABLE IF EXISTS BlogPost
+
+--CREATE TABLE BlogPost
+--(
+--	Id INT IDENTITY(1,1) PRIMARY KEY,
+--    Title NVARCHAR(50) NULL,
+--	Author NVARCHAR(50) NULL,
+--	Content NVARCHAR(1000) NULL,
+
+
+--)
+
+--DROP TABLE Tags
+
+
+
+--Create TABLE Tags (
+--	Id INT IDENTITY(1,1) PRIMARY KEY,
+--	PostId int,
+--	Name NVARCHAR(10),
+--	)
