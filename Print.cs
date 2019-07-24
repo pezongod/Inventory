f789c17d8e6e72c8305d465f3380e853d6a09f5e
Ciwifrukt <v.strandberg@outlook.com>
2019-07-24 11:59:36 +0200

    fixed printed methods

--- Print.cs
+++ Print.cs
@@ -9,11 +9,33 @@ namespace Inventory
         {
         }
 
-        internal void PrintPage(List<MenuItems> mainPageMenu)
+        internal void PrintPage(string head, List<MenuItems> PageMenu)
         {
-            PrintHeader(title);
-            PrintMenu();
-            MakeChoices();
+            PrintHeader(head);
+            Console.WriteLine("Vad vill du göra?");
+            PrintMenu(PageMenu);
+            MakeChoices(PageMenu);
+            
         }
+
+
+        private void PrintHeader(string head)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(head);
+            Console.ResetColor();
+        }
+        private void PrintMenu(List<MenuItems> PageMenu)
+        {
+            foreach (MenuItems item in PageMenu)
+            {
+                Console.WriteLine($"item.Name);
+            }
+        }
+        private void MakeChoices(List<MenuItems> pageMenu)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
