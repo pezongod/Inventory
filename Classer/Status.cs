82c7ce056d6f0649843afc822484c6373ff0f825
Ciwifrukt <v.strandberg@outlook.com>
2019-07-25 16:13:51 +0200

    df

--- Classer/Status.cs
+++ Classer/Status.cs
@@ -9,6 +9,9 @@ namespace Inventory.Classer
     public class Status: IEntity
     {
         public int Id { get; set; }
+        public int TypId { get; set; }
+
         public string Namn { get; set; }
+
     }
 }
