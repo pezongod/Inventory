88b3a5b9a27029a998e569977c5dd00f7bc699bf
pezongod <pezongod@gmail.com>
2019-07-25 11:26:00 +0200

    -

---
+++ Classer/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Classer
+{
+    public class Status: IEntity
+    {
+        public int Id { get; set; }
+        public string Namn { get; set; }
+    }
+}
